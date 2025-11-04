5ddc83ee9e7903175ebf0a4800603b74f36479d5
dam <daapariciopereira2@danielcastelao.org>
2025-11-04 12:17:12 +0100

    Commit ViewModel: Creacion de variables a emplear en la UI

--- app/src/main/java/gz/dam/trabajosimondize/ViewModel.kt
+++ app/src/main/java/gz/dam/trabajosimondize/ViewModel.kt
@@ -1,7 +1,28 @@
 package gz.dam.trabajosimondize
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MyViewModel() : ViewModel() {
     // Aquí iría la implementación del ViewModel
+    private val TAG_LOG = "miDebug"
+
+
+
+    // Estado del juego
+
+    val estadoLiveData: MutableLiveData<Estado> = MutableLiveData(Estado.INICIO)
+
+    // Secuencia de colores (0=Rojo,1=Verde,2=Azul,3=Amarillo)
+    private val secuencia = mutableListOf<Int>()
+    private var indiceJugador : MutableLiveData<Int> = MutableLiveData(0)
+
+    // Puntuación
+    val puntuacion: MutableLiveData<Int> = MutableLiveData(0)
+
+    // Botón activo para animación
+    val botonActivo: MutableLiveData<Int> = MutableLiveData(-1)
+
+    val ronda: MutableLiveData<Int> = MutableLiveData(0)
+
 }
