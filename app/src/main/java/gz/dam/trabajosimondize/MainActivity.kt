fbcd8e1a6ad6ec51f84ae20e15a33ec4d8532029
dam <daapariciopereira2@danielcastelao.org>
2025-11-14 10:40:00 +0100

    Commit ViewModel: Asignacion a la MainActivity la Interfaz

--- app/src/main/java/gz/dam/trabajosimondize/MainActivity.kt
+++ app/src/main/java/gz/dam/trabajosimondize/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,37 +12,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
 import gz.dam.trabajosimondize.ui.theme.TrabajoSimonDizeTheme
+import kotlin.getValue
 
 class MainActivity : ComponentActivity() {
+    private val miViewModel: MyViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TrabajoSimonDizeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+           Interfaz(miViewModel)
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrabajoSimonDizeTheme {
-        Greeting("Android")
     }
 }
